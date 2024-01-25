5b2f749cba21363d2736f12be2884cb9748a0cc4
Maxwel Robson <maxps2@live.com>
2024-01-25 20:04:13 -0300

    .

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -9,7 +9,7 @@ while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
     //se chute for igual ao número secreto
     if(chute == numeroSecreto) {
-        
+        break;
     } else {
         if(chute > numeroSecreto) {
              alert(`O número secreto é menor que o ${chute}`);
@@ -22,9 +22,12 @@ while (chute != numeroSecreto) {
 
 }
 
-if(tentaivas > 1) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
-} else {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
-}
+let palavraTentaiva = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+// if (tentaivas > 1) {
+//    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+//} else {
+//   alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+//}
 
