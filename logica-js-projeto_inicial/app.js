8d52e366f530e231a54c42d6c6dd006551e696a5
Maxwel Robson <maxps2@live.com>
2024-01-25 20:27:14 -0300

    .

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,29 +1,28 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 // enquanto chute não for igual ao número secreto
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt('Escolha um número entre 1 e 100');
     //se chute for igual ao número secreto
-    if(chute == numeroSecreto) {
+    if (chute == numeroSecreto) {
         break;
     } else {
-        if(chute > numeroSecreto) {
-             alert(`O número secreto é menor que o ${chute}`);
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
         } else {
-            alert(`O número secreto é maior que o ${chute}`);
+            alert(`O número secreto é maior que ${chute}`);
         }
          //tentativas = tentativas = 1
-         tentativas++;
+        tentativas++
     }
-
 }
 
-let palavraTentaiva = tentativas > 1 ? 'tentativas' : 'tentativa'
-alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
 
 // if (tentaivas > 1) {
 //    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
